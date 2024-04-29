119e115719d6db72c4ed957151b5629ab21f2ce4
Patrick Meade <pmeade@.edu>
2024-04-28 20:39:36 -0400

    Update

---
+++ public/testcode.js
@@ -0,0 +1,14 @@
+/* 
+</div>
+      <div className="Login">
+        <div className={'buttonContainer'}>
+          <input
+              className={'inputButton'}
+              type="button"
+              onClick={loginbutton}
+              value={loggedIn ? 'Log out' : 'Log in'}
+          />
+           {loggedIn ? <div>Your email address is {email}</div> : <div />}
+</div>
+
+*/
