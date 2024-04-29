b682f02ea2ffe341b9c006b07d80e350f126042d
Patrick Meade <pmeade@.edu>
2024-04-28 20:38:58 -0400

    Homepage front page change

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -64,18 +64,6 @@ const Homepage = (props) => {
       <div className="Sidebar">
         <Sidebar />
       </div>
-      <div className="Login">
-        <div className={'buttonContainer'}>
-          <input
-              className={'inputButton'}
-              type="button"
-              onClick={loginbutton}
-              value={loggedIn ? 'Log out' : 'Log in'}
-          />
-           {loggedIn ? <div>Your email address is {email}</div> : <div />}
-        </div>
-
-      </div>
     </div>
   );
 };
